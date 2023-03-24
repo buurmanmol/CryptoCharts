1fd1ffad8565c07dbe52c1602c383f9b97f6c8fa
Thomas Mol <thomas-mol@online.nl>
2023-03-24 13:13:48 +0100

    First

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import TradingViewWidget from './TradingViewWidget';
+
+function App() {
+  const [selectedCryptos, setSelectedCryptos] = useState(['BTC', 'ETH', 'ADA']);
+  const [selectedIndicators, setSelectedIndicators] = useState(['MACD']);
+
+  useEffect(() => {
+    const selectedCryptosString = JSON.stringify(selectedCryptos);
+    const selectedIndicatorsString = JSON.stringify(selectedIndicators);
+    localStorage.setItem('selectedCryptos', selectedCryptosString);
+    localStorage.setItem('selectedIndicators', selectedIndicatorsString);
+  }, [selectedCryptos, selectedIndicators]);
+
+  return (
+    <div className='App'>
+      <div className='header'>
+            <h2>How to use</h2>
+            <p>
+              Select the crypto symbols you want to see in the chart and the indicators you want to see in the chart.
+              The chart will automatically update when you select a new crypto symbol or indicator.
+              Your preferences will be saved for 30 days.
+            </p>
+            
+          </div>
+      <div className='chart-container'>
+        <TradingViewWidget symbols={selectedCryptos} indicators={selectedIndicators} />
+      </div>
+      <div className='explanation'>
+        <h2>How to read charts and use indicators?</h2>
+        <div className='explanation__section'>
+          <h2>MACD</h2>
+          <p>
+            The MACD indicator is a trend-following momentum indicator that shows the relationship between two moving
+            averages of prices. The MACD is calculated by subtracting the 26-period Exponential Moving Average (EMA) from
+            the 12-period EMA. A nine-day EMA of the MACD, called the "signal line", is then plotted on top of the MACD
+            line, which can function as a trigger for buy and sell signals. Traders may buy the security when the MACD
+            crosses above its signal line and sell—or short—the security when the MACD crosses below the signal line.
+          </p>
+      </div>
+      <div className='explanation__section'>
+        <h2>RSI</h2>
+        <p>
+          The Relative Strength Index (RSI) is a momentum oscillator that measures the speed and change of price
+          movements. The RSI is displayed as an oscillator (a line graph that moves between two extremes) and can have a
+          reading from 0 to 100. The RSI is considered overbought when above 70 and oversold when below 30.
+        </p>
+
+        <p>
+          Traders may buy the security when the RSI crosses above 30 and sell—or short—the security when the RSI crosses
+          below 70.
+        </p>
+
+        <p>
+          The RSI can also be used to identify the general trend. If the RSI is trending higher, prices are generally
+
+          increasing. If the RSI is trending lower, prices are generally decreasing.
+
+
+        </p>
+        </div>
+        <div className='explanation__section'>
+          <h2>VWAP</h2>
+          <p>
+            The Volume Weighted Average Price (VWAP) is a trading benchmark used by traders that gives the average price a
+            security has traded at throughout the day, based on both volume and price. The VWAP is calculated by adding
+            the dollars traded for every transaction (price multiplied by number of shares traded) and then dividing by
+            the total shares traded for the day. The VWAP is displayed as a line graph that moves throughout the trading
+            day.
+          </p>
+          </div>
+          </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/TradingViewWidget.jsx
@@ -0,0 +1,191 @@
+import React, { useEffect, useRef } from "react";
+import "./TradingViewWidget.css";
+import Cookies from "js-cookie";
+
+
+let tvScriptLoadingPromise;
+
+function CryptoChart({ symbol, indicators }) {
+  const onLoadScriptRef = useRef();
+  const containerIdRef = useRef(`tradingview_${symbol}`);
+
+  useEffect(() => {
+    onLoadScriptRef.current = createWidget;
+
+    if (!tvScriptLoadingPromise) {
+      tvScriptLoadingPromise = new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.id = "tradingview-widget-loading-script";
+        script.src = "https://s3.tradingview.com/tv.js";
+        script.type = "text/javascript";
+        script.onload = resolve;
+
+        document.head.appendChild(script);
+      });
+    }
+
+    tvScriptLoadingPromise.then(
+      () => onLoadScriptRef.current && onLoadScriptRef.current()
+    );
+
+    return () => (onLoadScriptRef.current = null);
+
+    function createWidget() {
+      if (
+        document.getElementById(containerIdRef.current) &&
+        "TradingView" in window
+      ) {
+        new window.TradingView.widget({
+          autosize: true,
+          symbol: `BINANCE:${symbol}`,
+          interval: "5",
+          timezone: "Etc/UTC",
+          theme: "dark",
+          style: "1",
+          locale: "en",
+          toolbar_bg: "#f1f3f6",
+          enable_publishing: false,
+          allow_symbol_change: true,
+          save_image: false,
+          studies: indicators,
+          container_id: containerIdRef.current,
+        });
+      }
+    }
+  }, [symbol, indicators]);
+
+  return (
+    <div className="tradingview-widget-container">
+      <div id={containerIdRef.current} Style="height:500px" />
+    </div>
+  );
+}
+
+export default function TradingViewWidget() {
+  const [cryptos, setCryptos] = React.useState([]);
+  const [indicators, setIndicators] = React.useState([]);
+  const cookieCryptos = Cookies.get("cryptos");
+  const cookieIndicators = Cookies.get("indicators");
+
+  React.useEffect(() => {
+    if (cookieCryptos) {
+      setCryptos(JSON.parse(cookieCryptos));
+    }
+
+    if (cookieIndicators) {
+      setIndicators(JSON.parse(cookieIndicators));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    Cookies.set("cryptos", JSON.stringify(cryptos));
+  }, [cryptos]);
+
+  React.useEffect(() => {
+    Cookies.set("indicators", JSON.stringify(indicators));
+  }, [indicators]);
+
+  const handleCryptoChange = (event) => {
+    const { value } = event.target;
+    if (value === "none") {
+      setCryptos([]);
+      return;
+    }
+
+    if (cryptos.includes(value)) {
+      setCryptos(cryptos.filter((c) => c !== value));
+    } else {
+      setCryptos([...cryptos, value]);
+    }
+  };
+
+  const handleIndicatorChange = (event) => {
+    const { value } = event.target;
+    if (value === "none") {
+      setIndicators([]);
+      return;
+    }
+
+    if (indicators.includes(value)) {
+      setIndicators(indicators.filter((i) => i !== value));
+    } else {
+      if (indicators.length >= 3) {
+        return;
+      }
+
+      setIndicators([...indicators, value]);
+    }
+  };
+
+  const cryptoOptions = [
+    { value: "BTCUSDT", label: "Bitcoin (BTC)" },
+    { value: "ETHUSDT", label: "Ethereum (ETH)" },
+    { value: "BNBUSDT", label: "Binance Coin (BNB)" },
+    { value: "ADAUSDT", label: "Cardano (ADA)" },
+    { value: "XRPUSDT", label: "XRP (XRP)" },
+    { value: "DOGEUSDT", label: "Dogecoin (DOGE)" },
+    { value: "SOLUSDT", label: "Solana (SOL)" },
+    { value: "DOTUSDT", label: "Polkadot (DOT)" },
+    { value: "LUNAUSDT", label: "Luna (LUNA)" },
+    { value: "LINKUSDT", label: "Chainlink (LINK)" },
+  ];
+
+  const indicatorOptions = [
+    { value: "STD;MACD", label: "MACD" },
+    { value: "STD;VWAP", label: "VWAP" },
+    { value: "STD;RSI", label: "RSI" },
+    { value: "STD;BollingerBands", label: "Bollinger Bands" },
+  ];
+
+  return (
+    <div className="tradingview-widget">
+      <div className="tradingview-widgets-grid">
+        <div className="col-md-3"Style="height:100%">
+          <div className="crypto-selector">
+            <h3>Select Cryptocurrencies</h3>
+            {cryptoOptions.map((option) => (
+              <div key={option.value} className="cryptoList">
+                <input
+                  type="checkbox"
+                  value={option.value}
+                  checked={cryptos.includes(option.value)}
+                  onChange={handleCryptoChange}
+                />
+                {option.label}
+                <br></br>
+              </div>
+            ))}
+          </div>
+  
+          <div className="indicator-selector">
+            <h3>Indicators (Max 3)</h3>
+            {indicatorOptions.map((option) => (
+              <div key={option.value} className="indicator-option">
+                <input
+                  type="checkbox"
+                  id={option.value}
+                  value={option.value}
+                  checked={indicators.includes(option.value)}
+                  onChange={handleIndicatorChange}
+                  disabled={
+                    indicators.length >= 3 && !indicators.includes(option.value)
+                  }
+                />
+  
+                <label htmlFor={option.value}>{option.label}</label>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-xl-9"Style="width:80%">
+            {cryptos.map((crypto, index) => (
+              <div key={crypto} Style="width:50%; display:inline-flex; flex-wrap:wrap;">
+                <CryptoChart symbol={crypto} indicators={indicators} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
